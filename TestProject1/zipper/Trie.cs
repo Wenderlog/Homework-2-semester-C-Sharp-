6b2f13f525e9c9d87203debb073f49cf28fb4f79
Wenderlog <kipop505g46@gmail.com>
2023-10-15 23:50:40 +0300

    LZV

---
+++ TestProject1/zipper/Trie.cs
@@ -0,0 +1,170 @@
+﻿namespace zipper;
+
+public class Trie<TValue>
+{
+    private static Value.None none = new Value.None();
+
+    private Dictionary<char, Trie<TValue>> next;
+
+    private Value value;
+
+    public Trie()
+    {
+        this.next = new Dictionary<char, Trie<TValue>>();
+        this.value = none;
+        this.Size = 0;
+    }
+
+    public Trie(IEnumerable<KeyValuePair<string, TValue>> data)
+    : this()
+    {
+        foreach (var (key, value) in data)
+        {
+            this[key] = value;
+        }
+    }
+
+    public int Size { get; private set; }
+
+    public TValue this[string key]
+    {
+        get => this.Get(key);
+        set => this.Add(key, value);
+    }
+
+    public bool Add(string key, TValue value)
+        => this.Set(key, value, 0);
+
+    public bool Contains(string key)
+        => this.Contains(key, 0);
+
+    public TValue Get(string key)
+        => this.Get(key, 0);
+
+    public bool Remove(string key)
+        => this.Remove(key, 0);
+
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        return this.HowManyStartsWithPrefix(prefix, 0);
+    }
+
+    private bool Set(string key, TValue value, int curIndex)
+    {
+        if (curIndex == key.Length)
+        {
+            bool wasTerminal = this.value != none;
+            this.value = new Value.Some(value);
+            if (!wasTerminal)
+            {
+                this.Size += 1;
+            }
+
+            return !wasTerminal;
+        }
+
+        char curChar = key[curIndex];
+
+        if (!this.next.ContainsKey(curChar))
+        {
+            this.next[curChar] = new Trie<TValue>();
+        }
+
+        bool wasAdded = this.next[curChar].Set(key, value, curIndex + 1);
+        if (wasAdded)
+        {
+            this.Size += 1;
+        }
+
+        return wasAdded;
+    }
+
+    private bool Contains(string key, int curIndex)
+    {
+        if (curIndex == key.Length)
+        {
+            return this.value != none;
+        }
+
+        char curChar = key[curIndex];
+        return this.next.ContainsKey(curChar)
+            ? this.next[curChar].Contains(key, curIndex + 1)
+            : false;
+    }
+
+    private TValue Get(string key, int curIndex)
+    {
+        if (curIndex == key.Length)
+        {
+            return this.value switch
+            {
+                Value.Some x => x.value,
+                _ => throw new KeyNotFoundException()
+            };
+        }
+
+        char curChar = key[curIndex];
+        return this.next.ContainsKey(curChar)
+            ? this.next[curChar].Get(key, curIndex + 1)
+            : throw new KeyNotFoundException();
+    }
+
+    private bool Remove(string key, int curIndex)
+    {
+        if (curIndex == key.Length)
+        {
+            bool wasTerminal = this.value != none;
+            this.value = none;
+            return wasTerminal;
+        }
+
+        char curChar = key[curIndex];
+
+        if (!this.next.ContainsKey(curChar))
+        {
+            return false;
+        }
+
+        Trie<TValue> nextTrie = this.next[curChar];
+
+        if (!nextTrie.Remove(key, curIndex + 1))
+        {
+            return false;
+        }
+
+        if (nextTrie.Size == 0)
+        {
+            this.next.Remove(curChar);
+        }
+
+        this.Size -= 1;
+
+        return true;
+    }
+
+    private int HowManyStartsWithPrefix(string prefix, int curIndex)
+    {
+        if (curIndex == prefix.Length)
+        {
+            return this.Size;
+        }
+
+        char curChar = prefix[curIndex];
+        return this.next.ContainsKey(curChar)
+            ? this.next[curChar].HowManyStartsWithPrefix(prefix, curIndex + 1)
+            : 0;
+    }
+
+    private record Value
+    {
+        public record None()
+        : Value();
+
+        public record Some(TValue value)
+        : Value();
+
+        private Value()
+        {
+        }
+    }
+}
---
+++ TestProject1/zipper/Workspace.cs
@@ -0,0 +1,90 @@
+﻿namespace zipper;
+
+public class Workspace
+{
+    public Workspace(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            string arg = args[i];
+            switch (arg)
+            {
+                case "-c":
+                    this.Mode = this.Mode == WorkMode.Undefined ? WorkMode.Encode : throw new WorkspaceException("Заданы два режима работы");
+                    break;
+                case "-u":
+                    this.Mode = this.Mode == WorkMode.Undefined ? WorkMode.Decode : throw new WorkspaceException("Заданы два режима работы");
+                    break;
+                case "-h":
+                case "--help":
+                    this.Mode = this.Mode == WorkMode.Undefined ? WorkMode.Help : throw new WorkspaceException("Заданы два режима работы");
+                    break;
+                case "-o":
+                    this.OutFile = i + 1 < args.Length ? args[++i] : throw new WorkspaceException("\"-o\" за аргументом должно следовать имя выходного файла");
+                    break;
+                default:
+                    this.InFile = arg;
+                    break;
+            }
+        }
+
+        if (this.InFile is null)
+        {
+            if (this.Mode != WorkMode.Help)
+            {
+                throw new WorkspaceException("Имя входного файла указано не было");
+            }
+
+            this.InFile = string.Empty;
+        }
+
+        if (this.OutFile is null)
+        {
+            this.OutFile = this.Mode switch
+            {
+                WorkMode.Encode => this.InFile + ".zipped",
+                WorkMode.Decode => this.InFile.Substring(this.InFile.Length - 7) == ".zipped"
+                    ? this.InFile.Substring(0, this.InFile.Length - 7)
+                    : throw new WorkspaceException("Не найдено имя выходного файла с именем входного файла, которое не заканчивается на .zipped"),
+                _ => string.Empty
+            };
+        }
+    }
+
+    public enum WorkMode
+    {
+        Encode,
+        Decode,
+        Help,
+        Undefined,
+    }
+
+    public string InFile { get; }
+    public string OutFile { get; }
+
+    public WorkMode Mode { get; } = WorkMode.Undefined;
+}
+
+[System.Serializable]
+public class WorkspaceException : System.Exception
+{
+    public WorkspaceException()
+    {
+    }
+
+    public WorkspaceException(string message)
+    : base(message)
+    {
+    }
+    public WorkspaceException(string message, System.Exception inner)
+    : base(message, inner)
+    {
+    }
+
+    protected WorkspaceException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+    : base(info, context)
+    {
+    }
+}
